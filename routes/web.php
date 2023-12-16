6c8fa0717327b8526bda04520744701aeed8f002
rizkimadya <rizkimadya88@gmail.com>
2023-12-16 21:37:22 +0800

    first commit

---
+++ routes/web.php
@@ -0,0 +1,109 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+Route::get('/login', function () {
+    return view('login');
+})->name("login");
+
+Route::get('/', function () {
+    return view('components.dashboard');
+})->name("dashboard");
+
+Route::group(['prefix' => 'permohonan-skt'],function(){
+
+    Route::get('/ormas-terdaftar', function(){
+        return view('components.permohonan-skt.skt');
+    })->name('permohonan-skt.ormas');
+
+    Route::get('/verifikasi', function(){
+        return view('components.permohonan-skt.verifikasi');
+    })->name('permohonan-skt.verifikasi');
+    
+    Route::get('/menunggu', function(){
+        return view('components.permohonan-skt.menunggu');
+    })->name('permohonan-skt.menunggu');
+});
+
+Route::group(['prefix' => 'permohonan-dana'],function(){
+
+    Route::get('/ormas-pemohon', function(){
+        return view('components.permohonan-dana.dana');
+    })->name('permohonan-dana.dana');
+
+    Route::get('/verifikasi', function(){
+        return view('components.permohonan-dana.verifikasi');
+    })->name('permohonan-dana.verifikasi');
+    
+    Route::get('/menunggu', function(){
+        return view('components.permohonan-dana.menunggu');
+    })->name('permohonan-dana.menunggu');
+});
+
+Route::group(['prefix' => 'pelaporan-kegiatan'],function(){
+
+    Route::get('/laporan-ormas', function(){
+        return view('components.pelaporan-kegiatan.laporan');
+    })->name('pelaporan-kegiatan.laporan');
+
+    Route::get('/verifikasi', function(){
+        return view('components.pelaporan-kegiatan.verifikasi');
+    })->name('pelaporan-kegiatan.verifikasi');
+});
+
+Route::group(['prefix' => 'dashboard-ormas'],function(){
+
+    Route::get('/status-skt', function(){
+        return view('components.dashboard-ormas.status');
+    })->name('dashboard-ormas.status');
+
+    Route::get('/keorganisasian', function(){
+        return view('components.dashboard-ormas.keorganisasian');
+    })->name('dashboard-ormas.keorganisasian');
+
+    Route::get('/kepengurusan', function(){
+        return view('components.dashboard-ormas.kepengurusan');
+    })->name('dashboard-ormas.kepengurusan');
+
+    Route::get('/dokumen', function(){
+        return view('components.dashboard-ormas.dokumen');
+    })->name('dashboard-ormas.dokumen');
+});
+
+Route::group(['prefix' => 'dashboard-default'],function(){
+
+    Route::get('/permohonanskt', function(){
+        return view('components.dashboard-default.permohonanskt');
+    })->name('dashboard-default.permohonanskt');
+});
+
+Route::group(['prefix' => 'dana-ormas'],function(){
+
+    Route::get('/permohonandana', function(){
+        return view('components.dana-ormas.permohonandana');
+    })->name('dana-ormas.permohonandana');
+});
+
+Route::group(['prefix' => 'kegiatan-ormas'],function(){
+
+    Route::get('/pelaporan', function(){
+        return view('components.kegiatan-ormas.pelaporan');
+    })->name('kegiatan-ormas.pelaporan');
+});
+// Route::get('permohonan-skt', function () {
+//     return view('components.permohonan-skt.skt');
+// })->name("skt");
+
+// Route::get('verifikasi', function () {
+//     return view('components.permohonan-skt.verifikasi');
+// })->name("verifikasi");
